8459af25a1ffc908766fb94634fbeb9d0678a147
amateraku <amateraku@gmail.com>
2022-02-05 20:05:35 +0200

    testing thread

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+from threading import Thread
+
+
+def second():
+    print(6543)
+    print(83724)
+
+
+def thirds():
+    print(4865)
+    print(46754)
+
+
+for i in range(100):
+    th = Thread(target=second(), args=(i, ))
+    th.start()
+    print(16534)
